9602bee724f4ee46ff05faa8b2b927f340bcdb69
Ari Asulin <ari.asulin@gmail.com>
2020-10-15 14:49:34 -0700

    image fixes

--- src/browser/asset/image/ImageAsset.js
+++ src/browser/asset/image/ImageAsset.js
@@ -13,8 +13,9 @@ export default class ImageAsset extends React.Component {
     render() {
         let i = this.props.i || 0;
         let className = 'asset-image';
-        if (i % 2 === 1)
-            className += ' odd';
+        className += [' even', ' odd'][i % 2];
+        // if(i % 4 === 0)
+        //     className += ' clear';
         return (
             <div className={className}>
                 <a href={this.props.url} target="_blank">
